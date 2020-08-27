95693a5d788f1cc5c4d7b726a62909c0c58877ee
Alexandre Brière <alexandre-1.briere@polymtl.ca>
2020-08-27 11:44:43 -0400

    reset
    reseting the project to make it cleaner

--- OpenGl barebone/src/header/Struct.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-struct ShaderSource {
-    std::string VertexSource;
-    std::string FragmentSource;
-};
--- OpenGl barebone/src/header/log.h
+++ OpenGl barebone/src/header/log.h
@@ -2,9 +2,16 @@
 #ifndef LOGMESSAGE
 #define LOGMESSAGE
 
-#include <string>
 #include <iostream>
-#include "Parameter.h"
+#include <string>
+#define ERROR_LEVEL ErrorLevel::INFOS
+
+enum class ErrorLevel {
+	CRITICAL = 0,
+	ERROR = 1,
+	WARNING = 2,
+	INFOS = 3,
+};
 
 class LogMessage
 {
@@ -12,22 +19,22 @@ public:
 	static const ErrorLevel LogLevel = ERROR_LEVEL;
 
 	static void critical(const std::string& message) {
-		if (LogLevel >= CRITICAL) {
+		if (LogLevel >= ErrorLevel::CRITICAL) {
 			std::cout << "\033[1m\033[31m" << message << "\033[0m" << std::endl;
 		}
 	};
 	static void error(const std::string& message){
-		if (LogLevel >= ERROR) {
+		if (LogLevel >= ErrorLevel::ERROR) {
 			std::cout << "\033[31m" << message << "\033[0m" << std::endl;
 		}
 	};
 	static void warning(const std::string& message) {
-		if (LogLevel >= WARNING) {
+		if (LogLevel >= ErrorLevel::WARNING) {
 			std::cout << "\033[33m" << message << "\033[0m" << std::endl;
 		}
 	};
 	static void infos(const std::string& message) {
-		if (LogLevel >= INFOS) {
+		if (LogLevel >= ErrorLevel::INFOS) {
 			std::cout << "\033[37m" << message << "\033[0m" << std::endl;
 		}
 	};
